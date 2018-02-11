9ab397510e1af77df4a07dd6c505054a199e098c
Dov Kruger <dov.kruger@gmail.com>
2018-02-11 18:36:59 -0500

    removed private directories

--- Errors/src/doubleto1024.java
+++ Errors/src/doubleto1024.java
@@ -1,6 +1,6 @@
-public class count1to10 {
+public class doubleto10 {
     public static void main(String[] a) {
-        for (int i = 1; i < 10; ++i)
+        for (int i = 0; i <= 1024; i *= 2)
             System.out.println(i);
     }
 }
