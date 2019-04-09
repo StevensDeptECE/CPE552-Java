cb6dcd4f80bfcbc2d2ba5e04f957754a790b3727
Dov Kruger <Dov.Kruger@gmail.com>
2019-04-09 17:55:47 -0400

    lists and hash maps

---
+++ InClassProjects2019S/ScannerTest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class ScannerTest {
+	public static void main(String[] args) throws Exception {
+		Scanner s = new Scanner(new FileReader("test.dat"));
+		// make the delimiter what you don't want?
+		while (s.hasNext()) {
+			String word = s.next();
+
+		}
+
+	}
