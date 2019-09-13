c5e9e503917d24748a041f93aa5a7fc37daaa7d5
Dov Kruger <Dov.Kruger@stevens.edu>
2019-09-13 17:23:00 -0400

    session03 object oriented!

---
+++ CPE552InClass2019F/src/Complex.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author dkruger
+ */
+public class Complex {
+    public static void main(String[] args) {
+    /*
+        Complex a = new Complex(1.5, 2.5); // 1.5 + 2.5i   (1.5,2.5)
+       Complex b = new Complex(1.6, 1.1);
+       Complex c = a.add(b);
+       Complex d = a.mult(b);
+       
+       System.out.println(c);
+       System.out.println(d);
+       System.out.println(d.neg());
+ */
+    }
+}
