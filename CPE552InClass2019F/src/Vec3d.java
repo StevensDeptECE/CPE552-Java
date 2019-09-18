55d2cb70035c1acadfae7b1b5c71ac81c176464f
Ziran Gong <gzr2008@vip.qq.com>
2019-09-18 17:04:06 -0400

    fix: Correct the variance and result

--- CPE552InClass2019F/src/Vec3d.java
+++ CPE552InClass2019F/src/Vec3d.java
@@ -33,10 +33,10 @@ public class Vec3d {
         Vec3d v1 = new Vec3d();
         System.out.println(v1); // (0,0,0)
         Vec3d v2 = new Vec3d(1,2,3);
-        System.out.println(v1); // (1,2,3)
+        System.out.println(v2); // (1,2,3)
         Vec3d v3 = new Vec3d(4,1,-1);
         System.out.println(add(v2,v3)); // (5,3,2)
         System.out.println(sub(v2,v3)); // (-3,1,4)
-        System.out.println(dot(v2,v3)); // 1*4+2*1+4*-1 = 2
+        System.out.println(dot(v2,v3)); // 1*4+2*1+3*-1 = 3
     }
 }
