80c865aabd05f3f7874a918d1ca15175b6292e8a
Dov Kruger <Dov.Kruger@stevens.edu>
2020-02-14 17:20:37 -0500

    added 3 examples strings, builtin types

---
+++ 2020S/Session05/src/TestStrings.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author dkruger
+ */
+public class TestStrings {
+    public static String addSpaces(String s) {
+        // java strings are IMMUTABLE (cannot change)
+        String ans = "";
+        for (int i = 0; i < s.length(); i++) {
+            ans += s.charAt(i);
+            ans += ' ';
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        String s = "this is a test";
+        s += "Testing";
+        System.out.println(s.charAt(2));
+        System.out.println(s.endsWith("in"));
+  
+        System.out.println(addSpaces(s));
+        //t h i s  i s  a  t e s t T e s t i n g
+//        System.out.println(removeVowels(s));//ths s  tstTstng
+//        System.out.println(reverse(s));//gnitseTtset a si siht
+//        System.out.println(replace(s, 'i', 'x')); //thxs xs a testTestxng
+    }
+}
