ccd2aa672560c9987e6a8cd7ad14b55598e2ec49
Dov Kruger <Dov.Kruger@stevens.edu>
2020-02-14 17:43:11 -0500

    fixed homework

---
+++ 2020S/Session05/src/Strings.java
@@ -0,0 +1,7 @@
+public class Strings {
+    public static void main(String[] args) {
+        System.out.println(removeVowels(s));//ths s  tstTstng
+        System.out.println(reverse(s));//gnitseTtset a si siht
+        System.out.println(replace(s, 'i', 'x')); //thxs xs a testTestxng
+    }
+}
