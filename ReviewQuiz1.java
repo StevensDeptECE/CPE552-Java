9ade2dc91c611eb3edfcf7761ce9054c22d68711
Dov Kruger <Dov.Kruger@stevens.edu>
2020-10-09 15:23:50 -0400

    review quiz1

---
+++ ReviewQuiz1.java
@@ -0,0 +1,37 @@
+public ______  ReviewQuiz1 {
+	_____ static void main(_______) {
+
+
+	}
+}
+
+interface A {
+	public void f() _____
+}
+
+________ B extends A {
+  public void g();
+}
+
+________ C implements A {
+
+}
+
+________ D implements A {
+	public void f() {
+		System.out.println("test");
+	}
+}
+
+________ E implements B {
+	public void f() {
+		System.out.println("test2");
+	}
+}
+
+________ F extends E {
+	public void g() {
+		System.out.println("test3");
+	}
+}
+		
